83a484bd76b716c1b17f5304ac5adef21c5ae76d
Wesley Friedman <friedman.wesley@gmail.com>
2016-08-01 22:56:59 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
-
+def my_collect(array)
+  stored_names = []
+  if block_given?
+    i = 0
+    while i < array.length
+      stored_names << yield(array[i])
+      i = i + 1
+    end
+    return stored_names
+  end
+end
